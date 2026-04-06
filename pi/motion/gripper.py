74dd560bcee1a00bd5a08de4cbc07669ad1cb8c0
Stewart Thompson <stewart.thompson@duke.edu>
2026-04-06 17:59:51 -0400

    Fix

--- pi/motion/gripper.py
+++ pi/motion/gripper.py
@@ -244,7 +244,7 @@ class GPIOGripper:
         # Bypassing the immediate target=0 check incase inertia takes a second to register
         time.sleep(1.0)
         
-        while self._get_ticks() < -2000:  # Minimum open distance
+        while self._get_ticks() < 2000:  # Minimum open distance
             if time.time() - start > _MOTION_TIMEOUT_S:
                 self._set_esc(_ESC_STOP)
                 raise RuntimeError(
